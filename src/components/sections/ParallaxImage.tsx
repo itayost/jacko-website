122572856a3f0c912e5e470f47a4d5048ba6e5d8
Itay Ostraich <itayost1@gmail.com>
2025-11-24 15:37:15 +0200

    Update ParallaxImage.tsx

--- src/components/sections/ParallaxImage.tsx
+++ src/components/sections/ParallaxImage.tsx
@@ -3,16 +3,20 @@
 
 const ParallaxImage = () => {
   return (
-    <section
-      className="w-full h-[600px] relative"
-      style={{
-        backgroundImage: 'url(/images/gallery/restaurant-view.jpg)',
-        backgroundAttachment: 'fixed',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover'
-      }}
-    />
+    <section className="w-full h-[600px] relative overflow-hidden">
+      {/* Background Image */}
+      <div
+        className="absolute inset-0 bg-center bg-no-repeat bg-cover md:bg-fixed"
+        style={{
+          backgroundImage: 'url(/images/gallery/restaurant-view.jpg)'
+        }}
+      />
+
+      {/* Gradient Overlay - Mobile only */}
+      <div className="absolute inset-0 z-10 md:hidden">
+        <div className="absolute inset-x-0 bottom-0 h-2/3 bg-gradient-to-t from-blue-primary via-blue-primary/70 to-transparent" />
+      </div>
+    </section>
   )
 }
 
